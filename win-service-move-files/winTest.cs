5910385afc1777f2bdfcd3552c0d66363f65eb65
marcos.casas.f@outlook.com <marcos.casas.f@outlook.com>
2020-07-21 14:31:30 -0400

    Insert into DB and Move file to processed folder

--- win-service-move-files/winTest.cs
+++ win-service-move-files/winTest.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,9 +54,9 @@ namespace win_service_move_files
                 DirectoryInfo PrPath = new DirectoryInfo(ProcessPath);
                 DirectoryInfo OuPath = new DirectoryInfo(OutPath);
                 //Move files
-                foreach(var file in InPath.GetFiles("*", SearchOption.AllDirectories))
+                foreach (var file in InPath.GetFiles("*", SearchOption.AllDirectories))
                 {
-                    if(File.Exists(PrPath + file.Name))
+                    if (File.Exists(PrPath + file.Name))
                     {
                         File.SetAttributes(PrPath + file.Name, FileAttributes.Normal);
                         File.Delete(PrPath + file.Name);
@@ -64,7 +66,6 @@ namespace win_service_move_files
                     File.Move(InPath + file.Name, PrPath + file.Name);
                     File.SetAttributes(PrPath + file.Name, FileAttributes.Normal);
                     File.Delete(InPath + file.Name);
-                    
 
                     if (File.Exists(PrPath + file.Name))
                     {
@@ -77,15 +78,97 @@ namespace win_service_move_files
                 }
 
                 EventLog.WriteEntry("Copying process completed");
-                //Método que lea el txt
 
+                foreach (var file in PrPath.GetFiles("*", SearchOption.AllDirectories))
+                {
+                    if (File.Exists(PrPath + file.Name))
+                    {
+                        string fileName = @PrPath + file.Name;
+                        InsertData(fileName);
+                        MoveToProcess(PrPath + file.Name, OuPath + file.Name);//enviar ruta destino
+                    }
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                EventLog.WriteEntry(ex.Message,EventLogEntryType.Error);
+                EventLog.WriteEntry(ex.Message, EventLogEntryType.Error);
             }
         }
 
-        public 
+        #region InsertData
+        private void InsertData(string fileName)
+        {
+            DataTable dataInput = new DataTable();
+            /*
+            DataColumn column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "Id";
+            column.AutoIncrement = true;
+            column.AutoIncrementSeed = 1;
+            column.AutoIncrementStep = 1;
+            dataInput.Columns.Add(column);
+            */
+            dataInput.Columns.AddRange(new DataColumn[4]
+            {
+                new DataColumn("Month_Countable",typeof(int)),
+                new DataColumn("Client_RUT", typeof(string)),
+                new DataColumn("Trx_Count", typeof(string)),
+                new DataColumn("Service_Code", typeof(string))
+            });
+            
+            string lines = File.ReadAllText(fileName);
+
+            foreach(string row in lines.Split('\r','\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dataInput.Rows.Add();
+                    int i = 0;
+                    foreach(string cell in row.Split(';'))
+                    {
+                        dataInput.Rows[dataInput.Rows.Count - 1][i] = cell.Trim();
+                        i++;
+                    }
+                }
+            }
+
+            string conn = ConfigurationManager.ConnectionStrings["DbConnect"].ConnectionString;
+            using(SqlConnection sql = new SqlConnection(conn))
+            {
+                using(SqlBulkCopy sqlBC = new SqlBulkCopy(sql))
+                {
+                    sqlBC.DestinationTableName = "dbo.tbl_Interface";
+                    sql.Open();
+                    sqlBC.WriteToServer(dataInput);
+                    sql.Close();
+                }
+            }
+        }
+        #endregion
+
+        #region MoveToProcessed
+        public void MoveToProcess(string originPath, string destinationPath)
+        {
+             if (File.Exists(destinationPath))
+             {
+                 File.SetAttributes(destinationPath, FileAttributes.Normal);
+                 File.Delete(destinationPath);
+             }
+
+             //File.Copy(InPath + file.Name, PrPath + file.Name);
+             File.Move(originPath, destinationPath);
+             File.SetAttributes(destinationPath, FileAttributes.Normal);
+             File.Delete(originPath);
+
+             if (File.Exists(destinationPath))
+             {
+                 EventLog.WriteEntry("File copied successful.", EventLogEntryType.Information);
+             }
+             else
+             {
+                 EventLog.WriteEntry("File can't copy.", EventLogEntryType.Information);
+             }
+        }
+        #endregion
     }
 }
